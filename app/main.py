2984890111706cadeb66d9e99567ed9a502e4e53
MzeeJXulu <mzeejxulu@gmail.com>
2025-08-22 17:07:48 +0200

    Delete app directory

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.analysis_engine import generate_signal
-
-app = FastAPI()
-
-@app.get("/signal")
-def signal(pair: str = "EURUSD"):
-    return generate_signal(pair)
